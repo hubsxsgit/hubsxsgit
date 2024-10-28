8424a25ef090d73832ddfeb35ee6b229dc0a5423
hubsxsgit <74sxs69@gmail.com>
2024-10-28 19:16:16 +0900

    Update script.js

--- login/script.js
+++ login/script.js
@@ -3,7 +3,7 @@
         var u = document.getElementById("username").value;
         var p = document.getElementById("password").value;
         if (u === "test" && p === "test") {
-            window.location.href = "https://hubsxsgit.github.io/hubsxsgit/";
+            window.location.href = "https://hubsxsgit.github.io/hubsxsgit/sxs/index.html";
         } else {
             alert("아이디 또는 비밀번호가 틀렸습니다.");
         }
