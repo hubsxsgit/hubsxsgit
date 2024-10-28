6c714d18aeb5d6729c21a52c535bf1e97e5a2364
hubsxsgit <74sxs69@gmail.com>
2024-10-28 19:15:35 +0900

    Create script.js

---
+++ login/script.js
@@ -0,0 +1,11 @@
+(function() {
+    document.getElementById("loginButton").onclick = function() {
+        var u = document.getElementById("username").value;
+        var p = document.getElementById("password").value;
+        if (u === "test" && p === "test") {
+            window.location.href = "https://hubsxsgit.github.io/hubsxsgit/";
+        } else {
+            alert("아이디 또는 비밀번호가 틀렸습니다.");
+        }
+    };
+})();
